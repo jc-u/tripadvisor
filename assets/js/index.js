1c2f1873735244f0cdd215256194fb19e68af308
jc-u <jcurbain.admin@gmail.com>
2023-10-27 17:29:40 +0200

    create form

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const connexion = document.getElementById("connexion");
+	const modal = document.querySelector(".modal");
+	const close = document.querySelector(".icon-times");
+	const body = document.querySelector("body");
+
+	connexion.addEventListener("click", () => {
+		modal.style.display = "flex";
+	});
+
+	close.addEventListener("click", () => {
+		modal.style.display = "none";
+	});
+	body.addEventListener("click", (e) => {
+		if (e.target.className === "modal") {
+			modal.style.display = "none";
+		}
+	});
+
+	document.querySelector("form").addEventListener("submit", async (e) => {
+		e.preventDefault();
+
+		const data = {
+			firstname: document.getElementById("firstname").value,
+			lastname: document.getElementById("lastname").value,
+			email: document.getElementById("email").value,
+			message: document.getElementById("message").value,
+		};
+
+		console.log(data);
+
+		const response = await axios.post("http://localhost:3000/form", data);
+		console.log(response);
+	});
+});
